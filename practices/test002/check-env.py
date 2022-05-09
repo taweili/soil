4ca749ea66ce6d921953707a6a897158a4d30eb2
David Li <taweili@gmail.com>
2022-05-09 21:57:25 +0800

    more on pytorch cuda

--- practices/test002/check-env.py
+++ practices/test002/check-env.py
@@ -1,10 +1,12 @@
+import cv2
+import skimage
 import tensorflow as tf
 import torch
-import skimage
-import cv2
 
 print("Tensorflow: ", tf.__version__)
 print("GPU: ", tf.config.list_physical_devices('GPU'))
 
 print("Pytorch: ", torch.__version__)
 print("GPU: ", torch.cuda.is_available())
+if (torch.cuda.is_available()):
+    print("Device count:", torch.cuda.device_count())
